b8a4deec52bb3ec85f9e3bb128271d4597298220
San Lin Htike <sanlinhtike@192.168.1.7>
2023-12-28 11:32:15 +0630

    update domain

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -1,24 +1,21 @@
-import { cache } from 'react'
-import { domain } from '../pub-domain';
+import { cache } from "react";
 export const getUsers = cache(async () => {
-
-    try {
-        const item = await fetch(`${domain}/api/user/`);
-        return await item.json()
-
-    } catch (error) {
-        console.log(error, 'Error from User Api')
-    }
-})
+  try {
+    const item = await fetch(`/api/user/`);
+    return await item.json();
+  } catch (error) {
+    console.log(error, "Error from User Api");
+  }
+});
 
 export async function getData(userId: string) {
-    const res = await fetch(`${domain}/api/user?id=${userId}`, {
-        cache: 'no-store'
-    })
+  const res = await fetch(`/api/user?id=${userId}`, {
+    cache: "no-store",
+  });
 
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-    return res.json()
+  return res.json();
 }
