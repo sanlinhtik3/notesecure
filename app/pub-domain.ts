3665f9a002a7183fef519da7578fd7566456d00f
San Lin Htike <sanlinhtike@Sans-MacBook-Air.local>
2024-01-09 13:47:50 +0630

    update domain

--- app/pub-domain.ts
+++ app/pub-domain.ts
@@ -1,1 +1,2 @@
-export let domain = process.env.NEXT_ENV !== "production" ? "http://localhost:3000" : "https://notesecure.vercel.app";
+// export let domain = process.env.NEXT_ENV !== "production" ? "http://localhost:3000" : "https://notesecure.vercel.app";
+export let domain = process.env.NEXT_ENV !== "production" ? "https://notesecure.vercel.app" : "https://notesecure.vercel.app";
