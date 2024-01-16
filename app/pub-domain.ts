d52cb0a3e865560e651563e448923368249397c9
San Lin Htike <sanlinhtike@192.168.1.6>
2024-01-16 10:57:07 +0630

    update ui

--- app/pub-domain.ts
+++ app/pub-domain.ts
@@ -1,2 +1,5 @@
-export let domain = process.env.NEXT_ENV !== "production" ? "http://localhost:3000" : "https://notesecure.vercel.app";
-// export let domain = process.env.NEXT_ENV !== "production" ? "https://notesecure.vercel.app" : "https://notesecure.vercel.app";
+// export let domain = process.env.NEXT_ENV !== "production" ? "http://localhost:3000" : "https://notesecure.vercel.app";
+export let domain =
+  process.env.NEXT_ENV !== "production"
+    ? "https://notesecure.vercel.app"
+    : "https://notesecure.vercel.app";
