1a4b1a421dfe961c2b190b315ea63a2ee9b04c9b
San Lin Htike <sanlinhtike@Sans-MacBook-Air.local>
2024-01-18 16:39:00 +0630

    ohh update

--- app/pub-domain.ts
+++ app/pub-domain.ts
@@ -2,6 +2,7 @@
 //   process.env.NEXT_ENV !== "production"
 //     ? "http://localhost:3000"
 //     : "https://notesecure.vercel.app";
+
 export let domain =
   process.env.NEXT_ENV !== "production"
     ? "https://notesecure.vercel.app"
